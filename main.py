1e511174da8e852f6b652bd82524ee77ce7969a4
TannerLeise <tale5311@colorado.edu>
2022-01-03 17:56:43 -0700

    added functionality for what the dog doin

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import discord
 import os
 from dotenv import load_dotenv
-
+import requests
+import json
 load_dotenv()
+
+
+
+
+#Helper functions for the Project----------------------------------------------
+def get_dog():
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
+    json_data = json.loads(response.text)
+    return(json_data['message'])
+
+
+
+
+
+#----------------------------------------------------------------------------
 
 
 client = discord.Client()
@@ -18,6 +34,11 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Yoooo whats up '  + str(message.author) + '!')
-        # ay whats good my slime
+
+    if message.content.startswith('ayo what the dog doin'):
+        dog = get_dog()
+        await message.channel.send(str(message.author) + ' This is what the dog doin!! :)')
+        await message.channel.send(dog)
+
 
 client.run(os.getenv('TOKEN'))
