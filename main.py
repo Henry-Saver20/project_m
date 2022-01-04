39678d770faf9f81e7883fe5ba5707eb1009a21e
Henry Saver <hesa1262@colorado.edu>
2022-01-03 22:35:40 -0700

    FFMpeg Added and Various Updates

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import discord
 import os
+from discord.utils import sleep_until
 from dotenv import load_dotenv
+from discord.ext import commands, tasks
+import time
+import asyncio
 
 load_dotenv()
 
+intents = discord.Intents().all()
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!',intents=intents)
 
 @client.event
 async def on_ready():
@@ -18,5 +23,51 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Yoooo whats up '  + str(message.author) + '!')
+    await client.process_commands(message)
+
+
+@client.command(name="letsgo")
+async def play_audio(ctx):
+        print("called")
+        # Gets voice channel of message author
+        voice_channel = ctx.author.voice.channel
+        channel = None
+        if voice_channel != None:
+            channel = voice_channel.name
+            vc = await voice_channel.connect()
+            p = r".\sounds\letsGo.mp3"
+            exe_p = r".\ffmpeg\bin\ffmpeg.exe"
+            vc.play(discord.FFmpegPCMAudio(executable=exe_p, source=p))
+            # Sleep while audio is playing.
+            while vc.is_playing():
+                time.sleep(5)
+            await vc.disconnect()
+        else:
+            await ctx.send(str(ctx.author.name) + "is not in a channel.")
+        # Delete command after the audio is done playing.
+        await ctx.message.delete()
+
+@client.command(name="sloppy")
+async def play_audio(ctx):
+        print("called")
+        # Gets voice channel of message author
+        voice_channel = ctx.author.voice.channel
+        channel = None
+        if voice_channel != None:
+            channel = voice_channel.name
+            vc = await voice_channel.connect()
+            p = r".\sounds\sloppy.mp3"
+            exe_p = r".\ffmpeg\bin\ffmpeg.exe"
+            vc.play(discord.FFmpegPCMAudio(executable=exe_p, source=p))
+            # Sleep while audio is playing.
+            while vc.is_playing():
+                time.sleep(5)
+            await vc.disconnect()
+        else:
+            await ctx.send(str(ctx.author.name) + "is not in a channel.")
+        # Delete command after the audio is done playing.
+        await ctx.message.delete()
 
 client.run(os.getenv('TOKEN'))
+
+
